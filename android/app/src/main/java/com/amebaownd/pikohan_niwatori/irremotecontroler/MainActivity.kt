34e6d15a4561ded0d283b9163a356a52496ac9dd
pikohan-suzuki <b1702996@planet.kanazawa-it.ac.jp>
2021-01-26 22:26:28 +0900

    change to using androidViewModel

--- android/app/src/main/java/com/amebaownd/pikohan_niwatori/irremotecontroler/MainActivity.kt
+++ android/app/src/main/java/com/amebaownd/pikohan_niwatori/irremotecontroler/MainActivity.kt
@@ -3,11 +3,11 @@ package com.amebaownd.pikohan_niwatori.irremotecontroler
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import com.amebaownd.pikohan_niwatori.irremotecontroler.ui.main.getViewModelFactory
+import com.amebaownd.pikohan_niwatori.irremotecontroler.util.getViewModelFactory
 
 class MainActivity :AppCompatActivity(){
 
-        val viewModel:MainViewModel by viewModels{getViewModelFactory()}
+        val viewModel:MainViewModel by viewModels{getViewModelFactory(application)}
 
         override fun onCreate(savedInstanceState:Bundle?){
             super.onCreate(savedInstanceState)
